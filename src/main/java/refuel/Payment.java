535ecd7ad9c4959b4626f3166ab3e6e34247f659
SeokHeeJeong <vssh77@gmail.com>
2020-10-16 21:23:26 +0900

    보상트랜젝션 추가

--- src/main/java/refuel/Payment.java
+++ src/main/java/refuel/Payment.java
@@ -1,9 +1,12 @@
 package refuel;
 
 import javax.persistence.*;
+
+import lombok.Data;
 import org.springframework.beans.BeanUtils;
 import java.util.List;
 
+@Data
 @Entity
 @Table(name="Payment_table")
 public class Payment {
@@ -13,7 +16,7 @@ public class Payment {
     private Long id;
     private Long reservationId;
     private String fuelType;
-    private Integer qty;
+    private int qty;
     private String customerId;
     private String stationId;
     private String reservationStatus;
@@ -39,80 +42,6 @@ public class Payment {
 
 
     }
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public Long getReservationId() {
-        return reservationId;
-    }
-
-    public void setReservationId(Long reservationId) {
-        this.reservationId = reservationId;
-    }
-    public String getFuelType() {
-        return fuelType;
-    }
-
-    public void setFuelType(String fuelType) {
-        this.fuelType = fuelType;
-    }
-    public Integer getQty() {
-        return qty;
-    }
-
-    public void setQty(Integer qty) {
-        this.qty = qty;
-    }
-    public String getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(String customerId) {
-        this.customerId = customerId;
-    }
-    public String getStationId() {
-        return stationId;
-    }
-
-    public void setStationId(String stationId) {
-        this.stationId = stationId;
-    }
-    public String getReservationStatus() {
-        return reservationStatus;
-    }
-
-    public void setReservationStatus(String reservationStatus) {
-        this.reservationStatus = reservationStatus;
-    }
-    public Long getPrice() {
-        return price;
-    }
-
-    public void setPrice(Long price) {
-        this.price = price;
-    }
-    public String getPaymentType() {
-        return paymentType;
-    }
-
-    public void setPaymentType(String paymentType) {
-        this.paymentType = paymentType;
-    }
-    public String getPaymentStatus() {
-        return paymentStatus;
-    }
-
-    public void setPaymentStatus(String paymentStatus) {
-        this.paymentStatus = paymentStatus;
-    }
-
-
 
 
 }
